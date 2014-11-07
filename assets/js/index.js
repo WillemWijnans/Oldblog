95a34e2fc3209a44e36a65f3d05e275d21d03071
willemwijnans <willemwijnans@gmail.com>
2014-11-07 12:18:19 -0500

    add new theme

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,98 +1,163 @@
 /**
- * Main JS file for Casper behaviours
+ * Main JS file for Phantom
  */
+var $post = $('.post'), 
+	$first = $('.post.first'), 
+	$last = $('.post.last'), 
+	$fnav = $('.fixed-nav'),
+	$postholder = $('.post-holder'),
+	$postafter = $('.post-after'),
+	$sitehead = $('#site-head');
 
-/* globals jQuery, document */
-(function ($, sr, undefined) {
+/*globals jQuery, document */
+(function ($) {
     "use strict";
+    function srcTo (el) {
+    	$('html, body').animate({
+			scrollTop: el.offset().top
+		}, 1000);
+    }
+    $(document).ready(function(){
+        $(".post-holder").fitVids();
+     
+        $postholder.each(function (e) {
+        	if(e % 2 != 0)
+        		$(this).css({
+                    'background': '#343434',
+                    'color'     : '#fff',
+                })
+        })
 
-    var $document = $(document),
+        $postafter.each(function (e) {
+        	var bg = $(this).parent().css('background-color')
+        	$(this).css('border-top-color', bg)
 
-        // debouncing function from John Hann
-        // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
-        debounce = function (func, threshold, execAsap) {
-            var timeout;
+        	if(e % 2 == 0)
+        		$(this).css('left', '6%')
 
-            return function debounced () {
-                var obj = this, args = arguments;
-                function delayed () {
-                    if (!execAsap) {
-                        func.apply(obj, args);
-                    }
-                    timeout = null;
-                }
+        })
+        
 
-                if (timeout) {
-                    clearTimeout(timeout);
-                } else if (execAsap) {
-                    func.apply(obj, args);
-                }
+        $('.btn.first').click( function () {
+        	srcTo ($first)
+        })
+        $('.btn.last').click( function () {
+        	srcTo ($last)
+        })
+        $('#header-arrow').click(function () {
+            srcTo ($first)
+        })
 
-                timeout = setTimeout(delayed, threshold || 100);
-            };
-        };
+        $('.post-title').each(function () {
+        	var t = $(this).text(),
+        	    index = $(this).parents('.post-holder').index();
+        	$fnav.append("<a class='fn-item' item_index='"+index+"'>"+t+"</a>")
 
-    $document.ready(function () {
+        	$('.fn-item').click(function () {
+        		var i = $(this).attr('item_index'),
+        			s = $(".post[item_index='"+i+"']")
 
-        var $postContent = $(".post-content");
-        $postContent.fitVids();
+        		$('html, body').animate({
+					scrollTop: s.offset().top
+				}, 400);
 
-        function updateImageWidth() {
-            var $this = $(this),
-                contentWidth = $postContent.outerWidth(), // Width of the content
-                imageWidth = this.naturalWidth; // Original image resolution
+        	})
+        })
 
-            if (imageWidth >= contentWidth) {
-                $this.addClass('full-img');
-            } else {
-                $this.removeClass('full-img');
-            }
-        }
+        $('.post.last').next('.post-after').hide();
+        $(window).scroll( function () {
+        	var w = $(window).scrollTop(),
+        		g = $sitehead.offset().top,
+        		h = $sitehead.offset().top + $(this).height()-100;
 
-        var $img = $("img").on('load', updateImageWidth);
-        function casperFullImg() {
-            $img.each(updateImageWidth);
-        }
+        	if(w >= g && w<=h) {
+        		$('.fixed-nav').fadeOut('fast')
+        	} else {
+                if($(window).width()>900)
+        		  $('.fixed-nav').fadeIn('fast')
+        	}
 
-        casperFullImg();
-        $(window).smartresize(casperFullImg);
+        	$post.each(function () {
+        		var f = $(this).offset().top,
+        			b = $(this).offset().top + $(this).height(),
+        			t = $(this).parent('.post-holder').index(),
+        		 	i = $(".fn-item[item_index='"+t+"']"),
+        		 	a = $(this).parent('.post-holder').prev('.post-holder').find('.post-after');
 
-        $(".scroll-down").arctic_scroll();
+        		 $(this).attr('item_index', t);
+
+        		if(w >= f && w<=b) {
+
+        			i.addClass('active');
+        			a.fadeOut('slow')
+        		} else {
+        			i.removeClass('active');
+        			a.fadeIn('slow')
+        		}
+        	})
+        });
+
+        $('li').before('<span class="bult icon-asterisk"></span>')
+        $('blockquote p').prepend('<span class="quo icon-quote-left"></span>')
+            .append('<span class="quo icon-quote-right"></span>')
 
     });
 
-    // smartresize
-    jQuery.fn[sr] = function(fn) { return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
+    $post.each(function () {
+        var postText = $(this).html();
+        var fa  = [];
+        for(var i=0; i < icons.length; i++) {
+            fa[i]       = {};
+            fa[i].str   = "@"+ icons[i]+ "@";
+            fa[i].icon  = icons[i];
+            fa[i].int   = postText.search(fa[i].str);
 
-    // Arctic Scroll by Paul Adam Davis
-    // https://github.com/PaulAdamDavis/Arctic-Scroll
-    $.fn.arctic_scroll = function (options) {
+            if(fa[i].int > -1 ) { 
+                fa[i].count = postText.match(new RegExp(fa[i].str,"g")).length;
+                console.log(fa[i].count)
+                for(var j=0; j < fa[i].count; j++) {
+                    $(this).html($(this).html().replace(fa[i].str, "<i class='fa "+fa[i].icon+"'></i>"))
+                }
+            }
+        }
+    })
+    
 
-        var defaults = {
-            elem: $(this),
-            speed: 500
-        },
-
-        allOptions = $.extend(defaults, options);
-
-        allOptions.elem.click(function (event) {
-            event.preventDefault();
-            var $this = $(this),
-                $htmlBody = $('html, body'),
-                offset = ($this.attr('data-offset')) ? $this.attr('data-offset') : false,
-                position = ($this.attr('data-position')) ? $this.attr('data-position') : false,
-                toMove;
-
-            if (offset) {
-                toMove = parseInt(offset);
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, allOptions.speed);
-            } else if (position) {
-                toMove = parseInt(position);
-                $htmlBody.stop(true, false).animate({scrollTop: toMove }, allOptions.speed);
+    
+    // Show/hide the top menu on scroll
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 100) {
+                $('.top-toggle-menu').fadeOut(200);
             } else {
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, allOptions.speed);
+                $('.top-toggle-menu').fadeIn(200);
             }
         });
 
-    };
-})(jQuery, 'smartresize');
+        $("#menu-toggle").click(function() {
+          $(this).toggleClass("open");
+          $("#top-menu").fadeToggle();
+        });
+
+    
+
+    // Show/hide the footer button
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 600) {
+            $('.go-top').fadeIn(200);
+        } else {
+            $('.go-top').fadeOut(200);
+        }
+    });
+
+    // Animate scrolling
+    $('.go-top').click(function(e) {
+        e.preventDefault();
+        
+        $('html, body').animate({scrollTop: 0}, 500);
+    });
+    
+
+
+}(jQuery));
+
+
